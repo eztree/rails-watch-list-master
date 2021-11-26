7c6f875a6fb3e889ba2986c76f1406d653551298
eztree <huienlim@gmail.com>
2021-11-26 12:19:19 +0800

    Rails watch list master

---
+++ code/eztree/rails-watch-list-master/rails-watch-list-master/config/routes.rb
@@ -0,0 +1,9 @@
+Rails.application.routes.draw do
+  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  root to: 'lists#index'
+  resources :lists, only: %w[show new create] do
+    resources :bookmarks, only: %w[create]
+  end
+
+  resources :bookmarks, only: %w[destroy]
+end
